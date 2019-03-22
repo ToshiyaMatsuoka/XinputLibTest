1582a933d26dbd063f03e3ac4ce9385d326cc013
ToshiyaMatsuoka <triger.1306@gmail.com>
2019-03-22 21:45:11 +0900

    過去のSoundLibのラッパークラスコード

---
+++ XinputLibTest/Sound.cpp
@@ -0,0 +1,93 @@
+﻿#include "Sound.h"
+
+
+
+Sound::Sound()
+{
+	m_soundsManager.Initialize();
+}
+
+
+Sound::~Sound()
+{
+	for (auto i : m_simultaneousKeys)
+	{
+		for (INT si = 0; si < SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX; ++si)
+		{
+			delete[](i.second.m_pKeys[si]);
+		}
+	}
+
+	m_simultaneousKeys.clear();
+}
+
+VOID Sound::AddFile(const TCHAR * pFilePath, const TCHAR * pKey)
+{
+	m_soundsManager.AddFile(pFilePath, pKey);
+}
+
+//　一気に10個分の音声オブジェクトの生成する
+VOID Sound::AddSimultaneousFile(const TCHAR * pFilePath, const TCHAR * pKey)
+{
+	size_t tCharLength = _tcsclen(pKey);
+	size_t additionalKeyLength = tCharLength + 3;	//! 数字と\0の数足す
+
+	for (INT i = 0; i < SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX; ++i)
+	{
+		m_simultaneousKeys[pKey].m_pKeys[i] = new TCHAR[additionalKeyLength];
+
+		_tcscpy_s(m_simultaneousKeys[pKey].m_pKeys[i], additionalKeyLength, pKey);
+
+		_itot_s(i, &m_simultaneousKeys[pKey].m_pKeys[i][tCharLength], sizeof(TCHAR) * (1 + (i / SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX)), 10);
+		m_simultaneousKeys[pKey].m_pKeys[i][tCharLength + 1 + (i / SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX)] = m_TEXT_END;
+
+		m_soundsManager.AddFile(pFilePath, &m_simultaneousKeys[pKey].m_pKeys[i][0]);
+	}
+}
+
+//一気に作った音声オブジェクトの再生
+VOID Sound::OneShotSimultaneous(const TCHAR * pKey)
+{
+	INT currentNum = m_simultaneousKeys[pKey].m_currentPlayNum;
+
+	INT prevPrevNum = currentNum - 2;
+
+	if (prevPrevNum < 0) prevPrevNum = SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX + prevPrevNum;
+
+	Stop(&m_simultaneousKeys[pKey].m_pKeys[prevPrevNum][0]);
+
+	OneShotStart(&m_simultaneousKeys[pKey].m_pKeys[currentNum][0]);
+
+	m_simultaneousKeys[pKey].m_currentPlayNum = (currentNum >= (SimultaneousKeys::m_SIMULTANEOUS_NUM_MAX - 1)) ? 0 : ++currentNum;
+}
+
+VOID Sound::LoopStart(const TCHAR * pKey)
+{
+	m_soundsManager.Start(pKey, true);
+}
+
+VOID Sound::OneShotStart(const TCHAR * pKey)
+{
+	m_soundsManager.Start(pKey, false);
+}
+
+VOID Sound::Pause(const TCHAR * pKey)
+{
+	m_soundsManager.Pause(pKey);
+}
+
+VOID Sound::Resume(const TCHAR * pKey)
+{
+	m_soundsManager.Resume(pKey);
+}
+
+VOID Sound::Stop(const TCHAR * pKey)
+{
+	m_soundsManager.Stop(pKey);
+}
+
+VOID Sound::SetVolume(const TCHAR * pKey, INT vol)
+{
+	m_soundsManager.SetVolume(pKey, vol);
+}
+
---
+++ XinputLibTest/Sound.h
@@ -0,0 +1,59 @@
+﻿#ifndef SOUND_H
+#define SOUND_H
+
+#include <windows.h>
+#include <tchar.h>
+
+#include <vector>
+#include <map>
+#include <array>
+
+#include "SoundLib/Include/SoundsManager.h"
+
+class Sound
+{
+public:
+	Sound();
+
+	~Sound();
+
+	VOID AddFile(const TCHAR* pFilePath, const TCHAR* pKey);
+
+	VOID AddSimultaneousFile(const TCHAR* pFilePath, const TCHAR* pKey);
+
+	VOID OneShotSimultaneous(const TCHAR* pKey);
+
+	VOID LoopStart(const TCHAR* pKey);
+
+	VOID OneShotStart(const TCHAR* pKey);
+
+	VOID Pause(const TCHAR* pKey);
+
+	VOID Resume(const TCHAR* pKey);
+
+	VOID Stop(const TCHAR* pKey);
+
+	VOID SetVolume(const TCHAR* pKey, INT vol);
+
+private:
+	struct SimultaneousKeys
+	{
+	public:
+		static const INT m_SIMULTANEOUS_NUM_MAX = 10;
+		TCHAR* m_pKeys[m_SIMULTANEOUS_NUM_MAX];
+		INT m_currentPlayNum = 0;
+	};
+
+	SoundLib::SoundsManager m_soundsManager;
+
+	const TCHAR m_TEXT_END = _T('\0');
+
+	std::map<const TCHAR*, SimultaneousKeys> m_simultaneousKeys;
+};
+
+//
+//bgm、seなどの振り分けを使用者ができるようにする
+//振り分けたそれぞれに停止と音量設定ができるようにする
+//
+
+#endif // !SOUND_H
