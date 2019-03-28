d92f3da0a538b00f3cf8d6fca5b504ef0486045e
ToshiyaMatsuoka <triger.1306@gmail.com>
2019-03-28 14:42:31 +0900

    関数コメント追加、10個オブジェクトを作成したか確認する処理の追加

--- XinputLibTest/Sound.cpp
+++ XinputLibTest/Sound.cpp
@@ -1,6 +1,6 @@
 ﻿#include "Sound.h"
 
-
+using namespace SoundLib;
 
 Sound::Sound()
 {
@@ -13,36 +13,33 @@ Sound::~Sound()
 	Stop();
 	for (auto i : m_simultaneousKeys)
 	{
-		for (int si = 0; si < SimultaneousKeys::SIMULTANEOUS_NUM_MAX; ++si)
+		for (auto& j : i.second.m_pKeys)
 		{
-			delete[](i.second.m_pKeys[si]);
+			delete[] j;
 		}
 	}
 	m_simultaneousKeys.clear();
 }
 
-SoundLib::PlayingStatus Sound::GetStatus(const WCHAR* pKey) const
+void Sound::AddFile(const TCHAR* pFilePath, const TCHAR* pKey, SoundType type)
 {
-	return m_soundsManager.GetStatus(pKey);
-}
-
-void Sound::AddFile(const TCHAR * pFilePath, const TCHAR * pKey, SoundType type)
-{
+	if (FindSameKey(pKey))return;
 	bool successAddFile = false;
 	successAddFile = m_soundsManager.AddFile(pFilePath, pKey);
 	if (successAddFile) {
 		SoundKey soundKey;
 		soundKey.Key = pKey;
 		soundKey.Type = type;
-		m_soundKey.emplace_back(soundKey);
+		m_soundKeys.emplace_back(soundKey);
 	}
 }
 
 //　一気に10個分の音声オブジェクトの生成する
-void Sound::AddSimultaneousFile(const TCHAR * pFilePath, const TCHAR * pKey, SoundType type)
+void Sound::AddSimultaneousFile(const TCHAR* pFilePath, const TCHAR* pKey, SoundType type)
 {
+	if (FindSameKey(pKey))return;
 	size_t keyLength = _tcsclen(pKey);
-	size_t additionalKeyLength = keyLength + 3;	//! 数字と\0の数足す
+	size_t additionalKeyLength = keyLength + 3;	// 数字と\0の数足す
 	bool successAddFile = false;
 
 	for (int i = 0; i < SimultaneousKeys::SIMULTANEOUS_NUM_MAX; ++i)
@@ -62,16 +59,21 @@ void Sound::AddSimultaneousFile(const TCHAR * pFilePath, const TCHAR * pKey, Sou
 		successAddFile = m_soundsManager.AddFile(pFilePath, m_simultaneousKeys[pKey].m_pKeys[i]);
 		if (successAddFile) {
 			SoundKey soundKey;
-			soundKey.Key = m_simultaneousKeys[pKey].m_pKeys[i];
+			soundKey.Key = pKey;
 			soundKey.Type = type;
-			m_soundKey.emplace_back(soundKey);
+			soundKey.isSimultaneous = true;
+			m_soundKeys.emplace_back(soundKey);
 		}
 	}
 }
 
 //一気に作った音声オブジェクトの再生
-void Sound::OneShotSimultaneous(const TCHAR * pKey)
+void Sound::OneShotSimultaneous(const TCHAR* pKey)
 {
+	if (false == FindSameKey(pKey))return;
+
+	if (false == IsSimultaneousKey(pKey)) return;
+
 	int currentNum = m_simultaneousKeys[pKey].m_currentPlayNum;
 
 	int prevPrevNum = currentNum - 2;
@@ -85,50 +87,96 @@ void Sound::OneShotSimultaneous(const TCHAR * pKey)
 	m_simultaneousKeys[pKey].m_currentPlayNum = (currentNum >= (SimultaneousKeys::SIMULTANEOUS_NUM_MAX - 1)) ? 0 : ++currentNum;
 }
 
-void Sound::LoopStart(const TCHAR * pKey)
+void Sound::LoopStart(const TCHAR* pKey)
 {
+	if (true == IsSimultaneousKey(pKey)) return;
 	m_soundsManager.Start(pKey, true);
 }
 
-void Sound::OneShotStart(const TCHAR * pKey)
+void Sound::OneShotStart(const TCHAR* pKey)
 {
+	if (true == IsSimultaneousKey(pKey)) return;
 	m_soundsManager.Start(pKey, false);
 }
 
-void Sound::Pause(const TCHAR * pKey)
+void Sound::Pause(const TCHAR* pKey)
 {
 	m_soundsManager.Pause(pKey);
 }
 
-void Sound::Resume(const TCHAR * pKey)
+void Sound::Resume(const TCHAR* pKey)
 {
 	m_soundsManager.Resume(pKey);
 }
 
-void Sound::Stop(const TCHAR * pKey)
+void Sound::Stop(const TCHAR* pKey)
 {
 	m_soundsManager.Stop(pKey);
 }
 
 void Sound::Stop(SoundType type)
 {
-	for (auto i : m_soundKey) {
+	for (auto i : m_soundKeys) {
 		if (type != i.Type && type != ALL_TYPE) continue;
 		m_soundsManager.Stop(i.Key);
 	}
 }
 
-void Sound::SetVolume(const TCHAR * pKey, int vol)
+PlayingStatus Sound::GetStatus(const TCHAR* pKey) const
+{
+	return m_soundsManager.GetStatus(pKey);
+}
+
+uint8_t Sound::GetVolume(const TCHAR* pKey) const
+{
+	return m_soundsManager.GetVolume(pKey);
+}
+
+float Sound::GetFrequencyRatio(const TCHAR* pKey) const
+{
+	return m_soundsManager.GetFrequencyRatio(pKey);
+}
+
+void Sound::SetFrequencyRatio(const TCHAR* pKey, float ratio)
+{
+	m_soundsManager.SetFrequencyRatio(pKey, ratio);
+}
+
+void Sound::SetVolume(int vol, const TCHAR* pKey)
 {
 	m_soundsManager.SetVolume(pKey, vol);
 }
 
-
 void Sound::SetVolume(int vol, SoundType type)
 {
-	for (auto i : m_soundKey) {
+	for (auto i : m_soundKeys) {
 		if (type != i.Type && type != ALL_TYPE) continue;
 		m_soundsManager.SetVolume(i.Key, vol);
 	}
 }
+
+bool Sound::IsSimultaneousKey(const TCHAR* pKey)
+{
+	for (auto i : m_soundKeys)
+	{
+		if (i.Key != pKey) continue;
+		if (i.isSimultaneous == true)
+		{
+			return true;
+		}
+		break;
+	}
+	return false;
+}
+
+bool Sound::FindSameKey(const TCHAR* pKey)
+{
+	for (auto i : m_soundKeys)
+	{
+		if (i.Key != pKey) continue;
+		return true;
+	}
+	return false;
+}
+
 
--- XinputLibTest/Sound.h
+++ XinputLibTest/Sound.h
@@ -18,39 +18,140 @@ public:
 		ALL_TYPE,
 		BGM,
 		SE,
+		VOICE,
 	};
 
 	struct SoundKey {
 		const TCHAR* Key;
 		SoundType Type;
+		bool isSimultaneous = false;
 	};
 
 	Sound();
 
 	~Sound();
 
-	SoundLib::PlayingStatus GetStatus(const WCHAR * pKey) const;
 
+	/// <summary>
+	/// 音声ファイルを追加する。
+	/// </summary>
+	/// <param name="pFilePath">ファイルパス</param>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <param name="type">音声に設定する音声種別</param>
 	void AddFile(const TCHAR* pFilePath, const TCHAR* pKey, SoundType type);
 
+	/// <summary>
+	/// 音声ファイルを10個分追加する。
+	/// </summary>
+	/// <param name="pFilePath">ファイルパス</param>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <param name="type">音声に設定する音声種別</param>
 	void AddSimultaneousFile(const TCHAR* pFilePath, const TCHAR* pKey, SoundType type);
 
+	/// <summary>
+	/// ファイルの先頭から単発再生を行う。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
 	void OneShotSimultaneous(const TCHAR* pKey);
 
+	/// <summary>
+	/// ファイルの先頭からループ再生を行う。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
 	void LoopStart(const TCHAR* pKey);
 
+	/// <summary>
+	/// ファイルの先頭から再生を行う。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
 	void OneShotStart(const TCHAR* pKey);
 
+	/// <summary>
+	/// 再生を一時停止する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>成否</returns>
+	/// <remarks>続きから再生再開するときには<see cref="Resume(const T*)"/>を実行して下さい。</remarks>
 	void Pause(const TCHAR* pKey);
 
+	/// <summary>
+	/// 一時停止中の音声を続きから再生する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>成否</returns>
+	/// <remarks><see cref="Pause(const T*)"/>で一時停止した音声の続きからの再生に使用します。</remarks>
 	void Resume(const TCHAR* pKey);
 
+	/// <summary>
+	/// 再生を停止する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>成否</returns>
+	/// <remarks>後で続きから再生する必要がある場合は、<see cref="Pause(const TCHAR*)"/>を使用して下さい。</remarks>
 	void Stop(const TCHAR* pKey);
 
+	/// <summary>
+	/// 再生を停止する。
+	/// </summary>
+	/// <param name="type">音声ファイルを識別する音声種別</param>
+	/// <returns>成否</returns>
+	/// <remarks>後で続きから再生する必要がある場合は、<see cref="Pause(const TCHAR*)"/>を使用して下さい。</remarks>
 	void Stop(SoundType type = ALL_TYPE);
 
-	void SetVolume(const TCHAR * pKey, int vol);
+	/// <summary>
+	/// 再生状況を示すステータスを取得する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>再生ステータス</returns>
+	SoundLib::PlayingStatus GetStatus(const TCHAR* pKey) const;
 
+	/// <summary>
+	/// ボリュームを取得する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>ボリューム(0:無音　100:音源ボリューム)</returns>
+	uint8_t GetVolume(const TCHAR* pKey) const;
+
+	/// <summary>
+	/// 再生速度とピッチの変化率を取得する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <returns>音源からの変化率</returns>
+	/// <remarks>
+	/// 1.0の場合、音源から変化なし。
+	/// 2.0の場合、再生速度2倍で1オクターブ高音。
+	/// 0.5の場合、再生速度1/2で1オクターブ低音。
+	/// </remarks>
+	float GetFrequencyRatio(const TCHAR* pKey) const;
+
+	/// <summary>
+	/// 再生速度とピッチの変化率を設定する。
+	/// </summary>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	/// <param name="ratio">音源からの変化率</param>
+	/// <returns>成否</returns>
+	/// <remarks>
+	/// <para>
+	/// 1.0の場合、音源から変化なし。
+	/// 2.0の場合、再生速度2倍で1オクターブ高音。
+	/// 0.5の場合、再生速度1/2で1オクターブ低音。
+	/// </para>
+	/// <para>設定可能最大値は4。</para>
+	/// </remarks>
+	void SetFrequencyRatio(const TCHAR* pKey, float ratio);
+
+	/// <summary>
+	/// ボリュームを設定する。
+	/// </summary>
+	/// <param name="vol">ボリューム(0:無音　100:音源ボリューム)</param>
+	/// <param name="pKey">音声ファイルを識別するキー</param>
+	void SetVolume(int vol,const TCHAR* pKey);
+
+	/// <summary>
+	/// ボリュームを設定する。
+	/// </summary>
+	/// <param name="vol">ボリューム(0:無音　100:音源ボリューム)</param>
+	/// <param name="type">音声ファイルを識別する音声種別</param>
 	void SetVolume(int vol, SoundType type = ALL_TYPE);
 
 private:
@@ -73,12 +174,21 @@ private:
 
 	std::map<const TCHAR*, SimultaneousKeys> m_simultaneousKeys;
 
-	std::vector<SoundKey> m_soundKey;
+	std::vector<SoundKey> m_soundKeys;
 
-	SoundType GetSoundType(int arrayNum) {
-		return m_soundKey[arrayNum].Type;
-	}
+	/// <summary>
+	/// AddSimultaneousFile()で生成したキーか調べる
+	/// </summary>
+	/// <param name="pKey"></param>
+	/// <returns>AddSimultaneousFile()で生成していれば真</returns>
+	bool IsSimultaneousKey(const TCHAR* pKey);
 
+	/// <summary>
+	/// 同じキーが存在するか確認する
+	/// </summary>
+	/// <param name="pKey"></param>
+	/// <returns>存在すれば真、しなければ偽</returns>
+	bool FindSameKey(const TCHAR* pKey);
 };
 
 //
